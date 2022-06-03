32b0151c9f8958e6f3ce3405a98c7d3c43eddea3
Denys <80554676+VestHaider@users.noreply.github.com>
2022-06-02 23:29:01 -0500

    New GUI Navigation Half Done

--- littlechef/app/src/main/java/comp3350/littlechef/presentation/HomeActivity.java
+++ littlechef/app/src/main/java/comp3350/littlechef/presentation/HomeActivity.java
@@ -3,13 +3,21 @@ package comp3350.littlechef.presentation;
 import comp3350.littlechef.R;
 import comp3350.littlechef.application.Main;
 import android.app.Activity;
-import android.content.Intent;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+import android.app.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
-public class HomeActivity extends Activity {
+public class HomeActivity extends Activity
+{
+    //initialize the fragments
+    BottomNavigationView bottomNavigationView;
+    ViewFragment viewFragment = new ViewFragment();
+    AddFragment addFragment = new AddFragment();
+    MealsFragment mealsFragment = new MealsFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +26,35 @@ public class HomeActivity extends Activity {
         Main.startUp();
 
         setContentView(R.layout.activity_home);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        replaceFragment(new ViewFragment()); //set view fragment on start
+
+        //set up bottom navigation listener
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener()
+        {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item)
+            {
+                switch (item.getItemId())
+                {
+                    case R.id.recipes:
+                        replaceFragment(new ViewFragment());
+                        return true;
+
+                    case R.id.add:
+                        replaceFragment(new AddFragment());
+                        return true;
+
+                    case R.id.meals:
+                        replaceFragment(new MealsFragment());
+                        return true;
+                }
+                return false;
+            }
+        });
+
+
     }
 
     @Override
@@ -27,20 +64,12 @@ public class HomeActivity extends Activity {
         Main.shutDown();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        return super.onOptionsItemSelected(item);
+    private void replaceFragment(Fragment fragment)
+    {
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
     }
 }
