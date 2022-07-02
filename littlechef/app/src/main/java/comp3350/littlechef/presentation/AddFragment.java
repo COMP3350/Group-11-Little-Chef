c8faac51a7085bc345f5f083b0ee1569a04d738a
cookb456 <100231347+cookb456@users.noreply.github.com>
2022-07-02 14:26:20 -0500

    add recipe fragment changes

--- littlechef/app/src/main/java/comp3350/littlechef/presentation/AddFragment.java
+++ littlechef/app/src/main/java/comp3350/littlechef/presentation/AddFragment.java
@@ -22,6 +22,7 @@ import comp3350.littlechef.objects.Recipe;
 //-----------------------------------------
 public class AddFragment extends Fragment
 {
+    //this is for adding new recipes
     String name, second, third;
 
     public AddFragment()
@@ -42,9 +43,9 @@ public class AddFragment extends Fragment
         View v = inflater.inflate(R.layout.fragment_add, container, false);
 
         //Get input from text fields
-        EditText nameField = (EditText) getActivity().findViewById(R.id.name_input);
-        EditText secondField = (EditText) getActivity().findViewById(R.id.secondField);
-        EditText thirdField= (EditText) getActivity().findViewById(R.id.thirdField);
+        EditText nameField = (EditText) v.findViewById(R.id.name_input);
+        EditText secondField = (EditText) v.findViewById(R.id.secondField);
+        EditText thirdField= (EditText) v.findViewById(R.id.thirdField);
 
         //button listener
         Button myButton = v.findViewById(R.id.addButton);
@@ -52,50 +53,25 @@ public class AddFragment extends Fragment
             @Override
             public void onClick(View v)
             {
+                name = nameField.getText().toString();
+                second = secondField.getText().toString();
+                third = thirdField.getText().toString();
 
-                Toast.makeText(getContext(), "Hello from fragment", Toast.LENGTH_SHORT).show();
+                //error checking on conversions??????
+                //recipe = new Recipe("Guacamole", 0, 30); //make a recipe
+                Recipe recipe = new Recipe(name, Integer.parseInt(second), Integer.parseInt(third));
+
+                Toast.makeText(getContext(), recipe.getName(), Toast.LENGTH_SHORT).show();
                 //TODO: add proper text that recipe was successfully added
+
+                //TODO: insert this blank recipe into DB
+
+                //TODO: create list of ingredients to add
+
+                //TODO create list of steps to add
             }
         });
 
         return v;
     }
-
-    //grabs input from add Recipe screen
-    private Recipe createRecipeFromEditText()
-    {
-        EditText nameField = (EditText) getActivity().findViewById(R.id.name_input);
-        EditText secondField = (EditText) getActivity().findViewById(R.id.secondField);
-        EditText thirdField= (EditText) getActivity().findViewById(R.id.thirdField);
-
-
-        //**** end of button listener
-        Button submitButton;
-        submitButton = (Button) getActivity().findViewById(R.id.addButton);
-
-        //setup listener for button clicking
-        submitButton.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                //set values from input boxes
-                //name = nameField.getText().toString();
-                //second = secondField.getText().toString();
-                //third = thirdField.getText().toString();
-                System.out.println("PRINTING!!!!");
-                Log.d("tag", "message");
-
-            }
-        }); //end listener
-        //****end of button listener
-
-        //error checking on conversions??????        //recipe = new Recipe("Guacamole", 0, 30); //make a recipe
-        Recipe recipe = new Recipe(name, Integer.parseInt(secondField.toString()), Integer.parseInt(thirdField.toString()) );
-
-        System.out.println("PRINTING!!!!");
-
-        return recipe;
-    }
-
 }
