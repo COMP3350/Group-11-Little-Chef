e9c337bdb780a2528958419247786bf5e66a2485
Dylan Elias <elias.dylan.m@gmail.com>
2022-07-02 16:54:59 -0500

    DBController implements Interface

--- littlechef/app/src/main/java/comp3350/littlechef/business/DBController.java
+++ littlechef/app/src/main/java/comp3350/littlechef/business/DBController.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import comp3350.littlechef.objects.Recipe;
 import comp3350.littlechef.persistence.PersistenceAccess;
 
-public class DBController {
+public class DBController implements PersistenceAccess{
 
     private PersistenceAccess service;
 
@@ -31,5 +31,36 @@ public class DBController {
     public PersistenceAccess getService()
     {
         return service;
+    }
+
+    @Override
+    public void open(String dbPath) {
+        service.open(dbPath);
+    }
+
+    @Override
+    public void close() { service.close();}
+
+    @Override
+    public String addRecipe(Recipe recipe) {
+        return service.addRecipe(recipe);
+    }
+
+    @Override
+    public Recipe updateRecipe(Recipe currRecipe) {
+        return service.updateRecipe(currRecipe);
+    }
+
+    @Override
+    public Recipe getRecipe(String name) {
+        return getService().getRecipe(name);
+    }
+
+    @Override
+    public ArrayList<Recipe> getListOfSameRecipe(Recipe recipe) { return service.getListOfSameRecipe(recipe); }
+
+    @Override
+    public boolean delRecipe(Recipe recipe) {
+        return delRecipe(recipe);
     }
 }
