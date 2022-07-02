08ee43830ed7d3a7665ff798f0408253183460cc
cookb456 <100231347+cookb456@users.noreply.github.com>
2022-07-02 10:15:42 -0500

    Input fields and fragment access

--- littlechef/app/src/main/java/comp3350/littlechef/presentation/AddFragment.java
+++ littlechef/app/src/main/java/comp3350/littlechef/presentation/AddFragment.java
@@ -5,7 +5,13 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+
+
+
+
 import comp3350.littlechef.R;
+import comp3350.littlechef.objects.Recipe;
 
 // CLASS: AddFragment.java
 //
@@ -14,6 +20,7 @@ import comp3350.littlechef.R;
 //
 //-----------------------------------------
 public class AddFragment extends Fragment
+
 {
     public AddFragment()
     {
@@ -26,11 +33,27 @@ public class AddFragment extends Fragment
         super.onCreate(savedInstanceState);
     }
 
-    @Override
+    //@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState)
     {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add, container, false);
+    }
+
+    //grabs input from add Recipe screen
+    private Recipe createCourseFromEditText()
+    {
+        EditText name = (EditText) getActivity().findViewById(R.id.name_input);
+        EditText secondField = (EditText) getActivity().findViewById(R.id.secondField);
+        EditText thirdField= (EditText) getActivity().findViewById(R.id.thirdField);
+        //recipe = new Recipe("Guacamole", 0, 30); //make a recipe
+
+
+        //error checking on conversions??????
+        Recipe recipe = new Recipe(name.toString(), Integer.parseInt(secondField.toString()), Integer.parseInt(thirdField.toString()) );
+
+
+        return recipe;
     }
 }
