7b6b405eb4b46cf4755400fe588fdde83c38fbf8
cookb456 <100231347+cookb456@users.noreply.github.com>
2022-07-04 01:56:28 -0500

    Added listveiw

--- littlechef/app/src/main/java/comp3350/littlechef/presentation/AddFragment.java
+++ littlechef/app/src/main/java/comp3350/littlechef/presentation/AddFragment.java
@@ -1,17 +1,32 @@
 package comp3350.littlechef.presentation;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.text.SpannableString;
+import android.text.style.StyleSpan;
+import android.text.style.UnderlineSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 import comp3350.littlechef.R;
+import comp3350.littlechef.business.AccessRecipes;
+import comp3350.littlechef.objects.Ingredient;
 import comp3350.littlechef.objects.Recipe;
+import comp3350.littlechef.objects.Unit;
+
+
 
 // CLASS: AddFragment.java
 //
@@ -22,6 +37,11 @@ import comp3350.littlechef.objects.Recipe;
 public class AddFragment extends Fragment
 {
     String name;
+    Recipe recipe;
+
+    private AccessRecipes accessRecipes;
+    private ArrayList<Recipe> recipeList;
+    private ArrayAdapter<Recipe> recipeArrayAdapter;
 
 
     public AddFragment()
@@ -45,6 +65,8 @@ public class AddFragment extends Fragment
 
     //TODO add check for duplicates
 
+    //TODO: fix amount entry
+
     //User creates recipe name
     //then user
     @Override
@@ -53,7 +75,8 @@ public class AddFragment extends Fragment
     {
         View v = inflater.inflate(R.layout.fragment_add, container, false);
 
-        Recipe recipe;
+        //just to get ride of name
+        //Recipe recipe = new Recipe("Blank");
 
         TextView bannerofIngredient = (TextView) v.findViewById(R.id.textViewIngred);
         EditText ingredName= (EditText) v.findViewById(R.id.ingredName);
@@ -73,6 +96,63 @@ public class AddFragment extends Fragment
         //Get input from text fields
         EditText recipeInput = (EditText) v.findViewById(R.id.nameInput);
 
+
+
+
+        //THIS ADDS A SMALL LIST VIEW TO ADD RECIPES
+
+
+        accessRecipes = new AccessRecipes();
+
+        recipeList = new ArrayList<Recipe>();
+        String result = accessRecipes.getRecipes(recipeList); // for testing
+
+
+        final ListView listView = (ListView) v.findViewById(R.id.existingRecipes);
+
+        recipeArrayAdapter = new ArrayAdapter<Recipe>(getActivity(), android.R.layout.simple_list_item_1, recipeList)
+        {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent)
+            {
+                Recipe recipe = getItem(position);
+                if(convertView == null)
+                {
+                    convertView = LayoutInflater.from(getContext()).inflate(R.layout.recipe_card_small,parent, false);
+                }
+                TextView name = (TextView) convertView.findViewById(R.id.recipeNameSmall);
+
+                SpannableString recipeNameFormatted = new SpannableString(recipe.getName());
+                recipeNameFormatted.setSpan(new UnderlineSpan(), 0, recipeNameFormatted.length(), 0);
+                recipeNameFormatted.setSpan(new StyleSpan(Typeface.BOLD), 0, recipeNameFormatted.length(), 0);
+
+                name.setText(recipeNameFormatted);
+
+
+                return convertView;
+            }
+        };
+
+        listView.setAdapter(recipeArrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+            {
+                Recipe selectedRecipe = (Recipe) listView.getItemAtPosition(position);
+
+                Intent detailedRecipe = new Intent(getActivity(), DetailedRecipeActivity.class);
+                detailedRecipe.putExtra("id", selectedRecipe); //pass the object reference to another activity
+                startActivity(detailedRecipe);
+            }
+        });
+
+
+
+
+
+
         //button listener for start Ingredients, will hide that button and show add ingredients stuff
         startIngredients.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -85,8 +165,29 @@ public class AddFragment extends Fragment
                 ingredButton.setVisibility(View.VISIBLE);
                 startIngredients.setVisibility(View.GONE);
 
-                recipe = new recipe(recipeInput);
+                recipe = new Recipe( recipeInput.toString() );
+                Toast.makeText(getContext(), "Add ingredients to the recipe!", Toast.LENGTH_SHORT).show();
 
+            }
+        });
+
+
+
+
+        //button listener for start Ingredients, will hide that button and show add ingredients stuff
+        ingredButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                //recipe.addIngredient(new Ingredient("Ranch Dressing", Unit.CUP, 0.25));
+
+
+                //TODO: fix units witj  ingredMeasurement
+                recipe.addIngredient(new Ingredient(ingredName.toString(), Unit.CUP, 0.25 ));
+
+                String recipeString = recipe.getName();
+
+                Toast.makeText(getContext(), recipeString, Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -98,11 +199,7 @@ public class AddFragment extends Fragment
 
 
 
-
-
-
-
-
+        //INSERT TO DB BUTTON
         //when reset button is clicked, wipe field boxes and hide again
         resetButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -122,7 +219,8 @@ public class AddFragment extends Fragment
                 ingredMeasurement.getText().clear();
                 ingredAmount.getText().clear();
 
-
+                //just for testing remove later!!!!
+                Toast.makeText(getContext(), "Added to db...wiping fields", Toast.LENGTH_SHORT).show();
 
             }
         });
