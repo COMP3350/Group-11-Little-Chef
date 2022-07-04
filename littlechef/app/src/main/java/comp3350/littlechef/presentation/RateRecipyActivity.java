1cc4042c8d9b04ab759efd55c4703c358ae115c6
Denys <80554676+VestHaider@users.noreply.github.com>
2022-07-04 12:10:42 -0500

    created a blank activity and added it to manifest

---
+++ littlechef/app/src/main/java/comp3350/littlechef/presentation/RateRecipyActivity.java
@@ -0,0 +1,14 @@
+package comp3350.littlechef.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+import comp3350.littlechef.R;
+import android.os.Bundle;
+
+public class RateRecipyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rate_recipy);
+    }
+}
