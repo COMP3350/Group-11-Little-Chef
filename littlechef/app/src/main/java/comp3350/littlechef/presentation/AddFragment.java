be9b11188f53b9dfed38b0bd95b08b0946685262
cookb456 <100231347+cookb456@users.noreply.github.com>
2022-07-03 23:16:22 -0500

    New changes from main

--- littlechef/app/src/main/java/comp3350/littlechef/presentation/AddFragment.java
+++ littlechef/app/src/main/java/comp3350/littlechef/presentation/AddFragment.java
@@ -30,7 +30,46 @@ public class AddFragment extends Fragment
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState)
     {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false);
+        View v = inflater.inflate(R.layout.fragment_add, container, false);
+
+        //Get input from text fields
+        EditText nameField = (EditText) v.findViewById(R.id.name_input);
+
+        //button listener for adding name
+        Button myButton = v.findViewById(R.id.addButton);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                name = nameField.getText().toString();
+
+                //error checking on conversions??????
+                //recipe = new Recipe("Guacamole", 0, 30); //make a recipe
+                Recipe recipe = new Recipe(name);
+
+                Toast.makeText(getContext(), "Created recipe!", Toast.LENGTH_SHORT).show();
+                //TODO: add proper text that recipe was successfully added
+
+                //TODO: insert this blank recipe into DB
+
+                //TODO: create list of ingredients to add
+
+                //TODO create list of steps to add
+            }
+        });
+
+
+        //button listener for adding ingredients
+        Button ingredButton = v.findViewById(R.id.ingredButton);
+        ingredButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                //TODO: implement add ingredient
+                Toast.makeText(getContext(), "Ingredient button", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return v;
     }
 }
