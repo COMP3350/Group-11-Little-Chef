747057b8878ecd58a6d629f47e507e95fa498d6a
cookb456 <100231347+cookb456@users.noreply.github.com>
2022-07-04 00:32:38 -0500

    Added buttons and text fields

--- littlechef/app/src/main/java/comp3350/littlechef/presentation/AddFragment.java
+++ littlechef/app/src/main/java/comp3350/littlechef/presentation/AddFragment.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import comp3350.littlechef.R;
@@ -21,6 +22,8 @@ import comp3350.littlechef.objects.Recipe;
 public class AddFragment extends Fragment
 {
     String name;
+
+
     public AddFragment()
     {
         // Required empty public constructor
@@ -32,50 +35,67 @@ public class AddFragment extends Fragment
         super.onCreate(savedInstanceState);
     }
 
+
+    //User creates recipe name
+    //then user
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState)
     {
         View v = inflater.inflate(R.layout.fragment_add, container, false);
 
+        Recipe recipe;
+
+        TextView bannerofIngredient = (TextView) v.findViewById(R.id.textViewIngred);
+        EditText ingredName= (EditText) v.findViewById(R.id.ingredName);
+        EditText ingredMeasurement= (EditText) v.findViewById(R.id.ingredMeasurement);
+        EditText ingredAmount= (EditText) v.findViewById(R.id.ingredAmount);
+        Button ingredButton= (Button) v.findViewById(R.id.ingredButton);
+        Button startIngredients= (Button) v.findViewById(R.id.startIngredients);
+
+        //Initial all the add ingredient stuff should not be visible until recipe object created, ie button clicked
+        bannerofIngredient.setVisibility(View.GONE);
+        ingredName.setVisibility(View.GONE);
+        ingredMeasurement.setVisibility(View.GONE);
+        ingredAmount.setVisibility(View.GONE);
+        ingredButton.setVisibility(View.GONE);
+
         //Get input from text fields
         EditText nameField = (EditText) v.findViewById(R.id.name_input);
 
         //button listener for adding name
-        Button myButton = v.findViewById(R.id.addButton);
-        myButton.setOnClickListener(new View.OnClickListener() {
+        ingredButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
-                name = nameField.getText().toString();
+            public void onClick(View v) {
 
-                //error checking on conversions??????
-                //recipe = new Recipe("Guacamole", 0, 30); //make a recipe
+                name = nameField.getText().toString();
+                //recipe = new Recipe("Guacamole"); //make a recipe
+
+                //if the recipe exists then just add ingredient to it, if not then create new recipe
                 Recipe recipe = new Recipe(name);
 
+                bannerofIngredient.setVisibility(View.VISIBLE);
+
+
+
+
+
                 Toast.makeText(getContext(), "Created recipe!", Toast.LENGTH_SHORT).show();
+
+
+
+
                 //TODO: add proper text that recipe was successfully added
 
                 //TODO: insert this blank recipe into DB
 
-                //TODO: create list of ingredients to add
-
                 //TODO create list of steps to add
-            }
-        });
 
-
-        //button listener for adding ingredients
-        Button ingredButton = v.findViewById(R.id.ingredButton);
-        ingredButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v)
-            {
-                //TODO: implement add ingredient
-                Toast.makeText(getContext(), "Ingredient button", Toast.LENGTH_SHORT).show();
+                //TODO add check for duplicates
             }
         });
 
         return v;
     }
+
 }
