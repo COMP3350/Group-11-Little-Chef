eccb27faab7e04792d2f088be7f4d56777f1b47c
Denys <80554676+VestHaider@users.noreply.github.com>
2022-07-06 10:54:20 -0500

    Update RateRecipyActivity.java

--- littlechef/app/src/main/java/comp3350/littlechef/presentation/RateRecipyActivity.java
+++ littlechef/app/src/main/java/comp3350/littlechef/presentation/RateRecipyActivity.java
@@ -68,7 +68,6 @@ public class RateRecipyActivity extends AppCompatActivity
 //            Intent i=new Intent(this, HomeActivity.class);
 //            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 //            startActivity(i);
-
         }
 
         else
