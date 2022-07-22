23ec7473a8e5f4df5d5caf74b9530d7cb9496aee
cookb456 <100231347+cookb456@users.noreply.github.com>
2022-07-22 13:17:25 -0500

    Fixes

--- littlechef/app/src/main/java/comp3350/littlechef/presentation/CalendarViewHolder.java
+++ littlechef/app/src/main/java/comp3350/littlechef/presentation/CalendarViewHolder.java
@@ -1,4 +1,28 @@
 package comp3350.littlechef.presentation;
 
-public class CalendarViewHolder {
+import android.view.View;
+import android.widget.TextView;
+import comp3350.littlechef.R;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CalendarViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
+{
+    public final TextView dayOfMonth;
+    private final CalendarAdapter.OnItemListener onItemListener;
+
+    public CalendarViewHolder(@NonNull View itemView, CalendarAdapter.OnItemListener onItemListener)
+    {
+        super(itemView);
+        dayOfMonth = itemView.findViewById(R.id.cellDayText);
+        this.onItemListener = onItemListener;
+        itemView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        onItemListener.onItemClick((getAdapterPosition()), (String) dayOfMonth.getText());
+    }
 }
