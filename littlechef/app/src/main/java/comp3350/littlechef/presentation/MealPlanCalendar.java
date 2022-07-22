3a4198ae8830d5e65b4cdd557b85d2b1631bc1a5
cookb456 <100231347+cookb456@users.noreply.github.com>
2022-07-22 16:25:59 -0500

    listview

--- littlechef/app/src/main/java/comp3350/littlechef/presentation/MealPlanCalendar.java
+++ littlechef/app/src/main/java/comp3350/littlechef/presentation/MealPlanCalendar.java
@@ -3,11 +3,32 @@ package comp3350.littlechef.presentation;
 
 import androidx.appcompat.app.AppCompatActivity;
 import comp3350.littlechef.R;
+import comp3350.littlechef.business.AccessRecipes;
+import comp3350.littlechef.objects.Recipe;
+
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.StyleSpan;
+import android.text.style.UnderlineSpan;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
 public class MealPlanCalendar extends AppCompatActivity
 {
+    private AccessRecipes accessRecipes;
+    private ArrayList<Recipe> recipeList;
+    private ArrayAdapter<Recipe> recipeArrayAdapter;
+    private Recipe recipe;
+
+    private ArrayList<Recipe> sundayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -15,6 +36,57 @@ public class MealPlanCalendar extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meal_plan);
 
+        accessRecipes = new AccessRecipes();
+        recipeList = new ArrayList<Recipe>();
+        String result = accessRecipes.getRecipes(recipeList);
+
+        //sundayList = new ArrayList<Recipe>();
+        //sundayList.add(recipeList.get(0));
+
+        if(result != null)
+        {
+            Messages.fatalError(this, result);
+        }
+        else
+        {
+            final ListView listView = (ListView) findViewById(R.id.sunday_list_view);
+
+            recipeArrayAdapter = new ArrayAdapter<Recipe>(this, android.R.layout.simple_list_item_1, recipeList)
+            {
+                @Override
+                public View getView(int position, View convertView, ViewGroup parent)
+                {
+                    recipe = getItem(position);
+                    if(convertView == null)
+                    {
+                        convertView = LayoutInflater.from(getContext()).inflate(R.layout.recipe_card,parent, false);
+                    }
+
+                    setValues(convertView);
+
+                    return convertView;
+                }
+            };
+            listView.setAdapter(recipeArrayAdapter);
+        }
+
+    }
+
+    private void setValues(View convertView)
+    {
+        String difficultyRating = recipe.getDifficultyRating();
+        String tasteRating = recipe.getTasteRating();
+
+        TextView name = (TextView) convertView.findViewById(R.id.recipe_name);
+        TextView estimatedTime = (TextView) convertView.findViewById(R.id.estimated_time);
+        TextView difficulty = (TextView) convertView.findViewById(R.id.difficulty);
+        TextView taste = (TextView) convertView.findViewById(R.id.taste);
+        TextView rating = (TextView) convertView.findViewById(R.id.rating);
+
+        estimatedTime.setText(recipe.getAverageCookingTime());
+        difficulty.setText(difficultyRating);
+        taste.setText(tasteRating);
+        rating.setText(recipe.getRatingString());
     }
 
 
