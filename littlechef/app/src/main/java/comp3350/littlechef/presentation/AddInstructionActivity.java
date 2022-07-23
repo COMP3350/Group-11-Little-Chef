4433c928a10946d24608579fc9695bcfd9c18627
KajalTomar <kajaltomar09@gmail.com>
2022-07-23 12:53:25 -0500

    done

--- littlechef/app/src/main/java/comp3350/littlechef/presentation/AddInstructionActivity.java
+++ littlechef/app/src/main/java/comp3350/littlechef/presentation/AddInstructionActivity.java
@@ -24,6 +24,14 @@ public class AddInstructionActivity extends AppCompatActivity
 {
     Recipe selectedRecipe;
 
+    String instruction;
+    String instructionSteps;
+
+    EditText instructionInput;
+    EditText instructionInputSteps;
+
+    private AccessRecipes accessRecipes;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -34,10 +42,18 @@ public class AddInstructionActivity extends AppCompatActivity
         selectedRecipe = (Recipe) previousIntent.getSerializableExtra("id");
 
         //Toast.makeText(this, "Add to " + selectedRecipe.getName(), Toast.LENGTH_SHORT).show();
-        Button addRecipeButton= (Button) findViewById(R.id.finishAddingInstruction);
+        Button finishAdding= (Button) findViewById(R.id.finishAdding);
+        Button addInstructionButton= (Button) findViewById(R.id.addInstructionButton);
+        Button cancelButton= (Button) findViewById(R.id.cancelButton);
+
+        instructionInput = (EditText) findViewById(R.id.instruction);
+        instructionInputSteps = (EditText) findViewById(R.id.instructionsSteps);
+
+        //for db
+        accessRecipes = new AccessRecipes();
 
         //button to go back to the main add recipe screen when done
-        addRecipeButton.setOnClickListener(new View.OnClickListener()
+        finishAdding.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View view)
@@ -46,5 +62,94 @@ public class AddInstructionActivity extends AppCompatActivity
             }
         });
 
+        //button listener for adding an instruction to a recipe
+        addInstructionButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                addInstruction();
+                //clear fields
+                instructionInput.getText().clear();
+                instructionInputSteps.getText().clear();
+            }
+        });
+
+        //button to cancel and delete working recipe
+        cancelButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                deleteRecipe();
+            }
+        });
+
     }//finish oCreate
+
+
+    //add instruction
+    private void addInstruction()
+    {
+        String result;
+
+        //get text from boxes
+        instruction = instructionInput.getText().toString().trim();
+        instructionSteps = instructionInputSteps.getText().toString().trim();
+
+        result = validateInstruction(instruction, instructionSteps);
+
+        if(result == null)
+        {
+            selectedRecipe.addInstructions(instruction, instructionSteps);
+
+            result = accessRecipes.updateRecipe(selectedRecipe);
+
+            if (result != null)
+            {
+                Messages.fatalError(this, result);
+            }
+            else
+            {
+                Toast.makeText(this, "Added instruction to " + selectedRecipe.getName(), Toast.LENGTH_SHORT).show();
+            }
+        }
+        else
+        {
+            Messages.warning(this, result);
+        }
+
+    }
+
+    private void deleteRecipe()
+    {
+        String result;
+        AccessRecipes accessRecipes = new AccessRecipes();
+        result = accessRecipes.deleteRecipe(selectedRecipe);
+        if (result == null)
+        {
+            finish();
+        }
+        else
+        {
+            Messages.fatalError(this, result);
+        }
+    }
+
+    //validate if proper instruction
+    private String validateInstruction(String instruction, String instructionSteps)
+    {
+        String result = null;
+
+        if (instruction == null || instructionSteps == null)
+        {
+            result = "Instructions cannot be null";
+        }
+        else if (instruction.trim().length() <= 0)
+        {
+            result = "Instructions cannot be empty";
+        }
+
+        return result;
+    }
 }
