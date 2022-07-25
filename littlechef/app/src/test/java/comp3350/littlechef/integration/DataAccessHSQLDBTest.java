d12ceacb1f0fa2ba4b9bc6024381321b7a262b0a
Dylan Elias <elias.dylan.m@gmail.com>
2022-07-24 19:09:15 -0500

    setup integration

--- littlechef/app/src/test/java/comp3350/littlechef/integration/DataAccessHSQLDBTest.java
+++ littlechef/app/src/test/java/comp3350/littlechef/integration/DataAccessHSQLDBTest.java
@@ -1,4 +1,37 @@
 package comp3350.littlechef.integration;
 
-public class DataAccessHSQLDBTest {
+import junit.framework.TestCase;
+
+import comp3350.littlechef.application.Main;
+import comp3350.littlechef.application.Services;
+import comp3350.littlechef.persistence.DataAccess;
+import comp3350.littlechef.persistence.DataAccessTest;
+
+public class DataAccessHSQLDBTest extends TestCase
+{
+    private static String dbName = Main.dbName;
+
+    public DataAccessHSQLDBTest(String arg0)
+    {
+        super(arg0);
+    }
+
+    public void testDataAccess()
+    {
+        DataAccess dataAccess;
+
+        Services.closeDataAccess();
+
+        System.out.println("\nStarting Integration test DataAccess (using default DB)");
+
+        // Use the following two statements to run with the real database
+        Services.createDataAccess(dbName);
+        dataAccess = Services.getDataAccess(dbName);
+
+        DataAccessTest.dataAccessTest(dataAccess);
+
+        Services.closeDataAccess();
+
+        System.out.println("Finished Integration test DataAccess (using default DB)");
+    }
 }
