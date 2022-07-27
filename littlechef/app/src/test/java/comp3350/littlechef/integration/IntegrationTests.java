56292636ef1f2e34df26b786f94e31a92c9aa32b
KajalTomar <kajaltomar09@gmail.com>
2022-07-26 21:09:57 -0500

    created the IntergratinTests class

---
+++ littlechef/app/src/test/java/comp3350/littlechef/integration/IntegrationTests.java
@@ -0,0 +1,18 @@
+package comp3350.littlechef.integration;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class IntegrationTests
+{
+    public static TestSuite suite;
+
+    public static Test suite()
+    {
+        suite = new TestSuite("Integration tests");
+        suite.addTestSuite(BusinessPersistenceSeamTest.class);
+        suite.addTestSuite(DataAccessHSQLDBTest.class);
+        return suite;
+    }
+}
+
